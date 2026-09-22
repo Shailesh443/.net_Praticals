f659cf235d4ef9af4f83a2e9a812693010f7c3f6
Shailesh Kumar Yadav <shailesh72095@gmail.com>
2026-09-22 11:17:50 +0530

    Practical 6 Added

---
+++ Practical_6/Practical_6/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Practical_6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Practical_6.Models;
+
+namespace Practical_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    Category = "Electronics",
+                    Price = 55000
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    Name = "Mobile",
+                    Category = "Electronics",
+                    Price = 25000
+                },
+
+                new Product
+                {
+                    Id = 3,
+                    Name = "Headphones",
+                    Category = "Accessories",
+                    Price = 2000
+                },
+
+                new Product{
+                    Id = 4,
+                    Name = "Shoes",
+                    Category = "Footwear",
+                    Price = 3000    
+            },
+                new Product{
+                    Id = 5,
+                    Name = "Watch",
+                    Category = "Accessories",
+                    Price = 5000
+                }
+            };  
+
+            return View(products);
+ 
+        }
+    }
+}
